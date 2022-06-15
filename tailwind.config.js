26b1d6ac69b208c0bb7b1133bc4782f9c4e7b470
umair <umairapi1995@gmail.com>
2022-06-15 23:23:37 +0500

    final rating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'overpass':['Overpass', 'sans-serif']
+      },
+      colors: {
+        primary: '#fb7413',
+        light_grey: '#959eac', // #959eac
+        medium_grey: '#7c8798',// #7c8798
+        dark_blue: '#252d37',
+        very_dark_blue:'#121417',
+      },
+    },
   },
   plugins: [],
 }
